5dcb7bb6276bf2c2bee61bf7589bd18c49c7bd32
Mario Domenech Goulart <mario.goulart@gmail.com>
2012-06-18 21:22:10 -0300

    srfi-16.scm: use the test egg

--- tests/srfi-16.scm
+++ tests/srfi-16.scm
@@ -1,42 +1,35 @@
 ; Test suite for SRFI-16
 ; 2004-01-01 / lth
 
-;(cond-expand (srfi-16))
+(use test)
 
-(define (writeln . xs)
-  (for-each display xs)
-  (newline))
-
-(define (fail token . more)
-  (writeln "Error: test failed: " token)
-  #f)
+(test-begin "srfi-16")
 
 (define plus
-  (case-lambda 
+  (case-lambda
    (() 0)
    ((x) x)
    ((x y) (+ x y))
    ((x y z) (+ (+ x y) z))
    (args (apply + args))))
 
-(or (equal? 0 (plus))
-    (fail 'plus:1))
-(or (equal? 1 (plus 1))
-    (fail 'plus:2))
-(or (equal? 6 (plus 1 2 3))
-    (fail 'plus:3))
-(or (equal? 55 (apply plus '(1 2 3 4 5 6 7 8 9 10)))
-    (fail 'plus:4))
+(test 0 (plus))
 
-(or (not (call-with-current-continuation
-	  (lambda (abort)
+(test 1 (plus 1))
+
+(test 6 (plus 1 2 3))
+
+(test 55 (apply plus '(1 2 3 4 5 6 7 8 9 10)))
+
+(test #f (call-with-current-continuation
+          (lambda (abort)
             (handle-exceptions exn
               (abort #f)
-	      ((case-lambda 
-		((a) a)
-		((a b c) (* a b c)))
-	       1 2)))))
-    (fail 'argmatch))
+              ((case-lambda
+                 ((a) a)
+                 ((a b c) (* a b c)))
+               1 2)))))
 
-(writeln "Done.")
+(test-end "srfi-16")
 
+(test-exit)
