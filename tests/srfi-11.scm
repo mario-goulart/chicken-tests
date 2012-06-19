6d56b41edf73585385004d236ce7ed7e97f9be52
Mario Domenech Goulart <mario.goulart@gmail.com>
2012-06-18 21:10:26 -0300

    srfi-11.scm: use the test egg

--- tests/srfi-11.scm
+++ tests/srfi-11.scm
@@ -1,36 +1,30 @@
 ; Test suite for SRFI-11
 ; 2004-01-01 / lth
 
-; (cond-expand (srfi-11))
+(use test)
 
-(define (writeln . xs)
-  (for-each display xs)
-  (newline))
+(test-begin "srfi-11")
 
-(define (fail token . more)
-  (writeln "Error: test failed: " token)
-  #f)
+(test '(1 2 (3 4))
+      (let-values (((a b . c) (values 1 2 3 4)))
+        (list a b c)))
 
-(or (equal? '(1 2 (3 4))
-	    (let-values (((a b . c) (values 1 2 3 4)))
-	      (list a b c)))
-    (fail 'let-values:1))
-(or (equal? '(x y a b)
-	    (let ((a 'a) (b 'b) (x 'x) (y 'y))
-	      (let-values (((a b) (values x y))
-			   ((x y) (values a b)))
-                 (list a b x y))))
-    (fail 'let-values:2))
-(or (equal? '(1 2 3 4)
-	    (let-values ((l (values 1 2 3 4)))
-	      l))
-    (fail 'let-values:3))
+(test '(x y a b)
+      (let ((a 'a) (b 'b) (x 'x) (y 'y))
+        (let-values (((a b) (values x y))
+                     ((x y) (values a b)))
+          (list a b x y))))
 
-(or (equal? '(x y x y)
-	    (let ((a 'a) (b 'b) (x 'x) (y 'y))
-	      (let*-values (((a b) (values x y))
-                            ((x y) (values a b)))
-                (list a b x y))))
-    (fail 'let*-values:1))
+(test '(1 2 3 4)
+      (let-values ((l (values 1 2 3 4)))
+        l))
 
-(writeln "Done.")
+(test '(x y x y)
+      (let ((a 'a) (b 'b) (x 'x) (y 'y))
+        (let*-values (((a b) (values x y))
+                      ((x y) (values a b)))
+          (list a b x y))))
+
+(test-end "srfi-11")
+
+(test-exit)
